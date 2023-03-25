cee8b551897766be0f8d3013811ee12577a830ae
yewon97 <dpdnjs9936@gmail.com>
2023-03-25 22:08:22 +0900

    generateStaticParams

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -12,3 +12,10 @@ export default function PantsPage({params} : Props) {
 		<div>{params.slug} 설명</div>
 	)
 }
+
+export function generateStaticParams() {
+	const products = ['pants', 'skirt'];
+	return products.map(product => ({
+		slug : product
+	}))
+}
