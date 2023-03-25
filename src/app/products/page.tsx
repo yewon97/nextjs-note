975ca8db705f2975130ce2e6ba966e4f8f4f9417
yewon97 <dpdnjs9936@gmail.com>
2023-03-26 01:06:22 +0900

    Link Challenge

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Produce() {
 	return (
-		<div>Produce</div>
+		<>
+			<div>Produce</div>
+			<ul>
+				<li><Link href="/products/shirt">shirt</Link></li>
+				<li><Link href="/products/pants">pants</Link></li>
+				<li><Link href="/products/skirt">skirt</Link></li>
+				<li><Link href="/products/shoes">shoes</Link></li>
+			</ul>
+		</>
 	)
 }
