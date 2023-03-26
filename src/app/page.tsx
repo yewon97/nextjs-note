ebf6e822d2c6d96222cbbaacd951ffd7471066bf
yewon97 <dpdnjs9936@gmail.com>
2023-03-26 20:56:47 +0900

    서버 컴포넌트와 클라이언트 컴포넌트의 차이점

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
 import { notFound } from 'next/navigation';
+import os from 'os'; // 노드 APIs
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+	console.log('안녕');
+	console.log(os.hostname());
+
+	const [naem, setNaem] = useState('');
+
   return <h1>홈페이지다!!!</h1>
 }
