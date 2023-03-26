9f609e0a52276ca93c39dc23eb3d83f7d3812eba
yewon97 <dpdnjs9936@gmail.com>
2023-03-26 22:32:47 +0900

    리팩토링 (설계)

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getProduct, getProducts } from '@/service/products';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
@@ -13,17 +14,27 @@ export function generateMetadata({ params } : Props) {
 	}
 }
 
-export default function PantsPage({params} : Props) {
-	if(params.slug === 'nothing') {
+export default function PantsPage({params : {slug}} : Props) {
+/* 	if(params.slug === 'nothing') {
+		notFound();
+	} */
+
+	// 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
+
+	const product = getProduct(slug);
+
+	if(!product) {
 		notFound();
 	}
+
 	return (
-		<div>{params.slug} 설명</div>
+		<div>{product} 설명</div>
 	)
 }
 
 export function generateStaticParams() {
-	const products = ['pants', 'skirt'];
+	// 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+	const products = getProducts();
 	return products.map(product => ({
 		slug : product
 	}))
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,13 @@
+import { getProducts } from '@/service/products'
 import Link from 'next/link'
 import React from 'react'
 
-const products = ['shirt','pants','skirt','shoes']
-
 export default function Produce() {
+
+	// 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+
+	const products = getProducts();
+
 	return (
 		<>
 			<div>Produce</div>
