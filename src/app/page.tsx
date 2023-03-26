f94707a3ea48b02e17200364c9359665fc1328c6
yewon97 <dpdnjs9936@gmail.com>
2023-03-26 21:55:48 +0900

    클라이언트 컴포넌트 동작원리 공부

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,20 @@ import { Inter } from '@next/font/google';
 import styles from './page.module.css';
 import { notFound } from 'next/navigation';
 import os from 'os'; // 노드 APIs
+import Counter from '@/components/Counter';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-	console.log('안녕');
+	console.log('안녕 -- 서버');
 	console.log(os.hostname());
 
-	const [naem, setNaem] = useState('');
+	// const [naem, setNaem] = useState(''); --> 에러발생
 
-  return <h1>홈페이지다!!!</h1>
+  return (
+		<>
+			<h1>홈페이지입니다.</h1>
+			<Counter />
+		</>
+	);
 }
