fd20ccd724d1c94fe1174ba158aaa16fbe3657ae
yewon97 <dpdnjs9936@gmail.com>
2023-03-27 21:54:45 +0900

    예전 방식의 API 라우트

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getProducts, Product } from '@/service/products'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Product[]>
+) {
+	if(req.method === 'GET') {
+		const products = await getProducts();
+		return res.status(200).json(products);
+	}
+
+  res.status(200);
+}
