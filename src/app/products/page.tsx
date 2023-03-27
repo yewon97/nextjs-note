d2265f83b8655e330c3bdcbd1e39b5eb18a5c09f
yewon97 <dpdnjs9936@gmail.com>
2023-03-27 20:48:33 +0900

    fetch를 이용해서 SSR, ISR, SSR하는 방법

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,30 @@
 import { getProducts } from '@/service/products'
 import Link from 'next/link'
 import React from 'react'
+import styles from './page.module.css'
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function Produce() {
 
 	// 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
 
 	const products = await getProducts();
+
+	const factText = await fetch("https://meowfacts.herokuapp.com", {
+										next : {revalidate : 3}
+										// revalidate를 0으로 하면 SSR
+										// 3으로 하면 ISR
+										// cache : '' --> SSG || 'no-store' -> SSR
+									})
+									.then(res => res.json())
+									.then((data) => {
+										return data.data[0];
+									})
+	
+	/* const res =  await fetch("https://meowfacts.herokuapp.com");
+	const data = await res.json();
+	const factText = data.data[0]; */
 
 	return (
 		<>
@@ -22,6 +38,7 @@ export default async function Produce() {
 					))
 				}
 			</ul>
+			<article className={styles.article}>{factText}</article>
 		</>
 	)
 }
