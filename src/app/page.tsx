7159c3c79027358f2699c8d8b3785ecd1391c8de
yewon97 <dpdnjs9936@gmail.com>
2023-03-28 23:28:39 +0900

    ver3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
 		<>
-			<h1>홈페이지입니다. ver2!!!!</h1>
+			<h1>홈페이지입니다. ver3!!!!</h1>
 			<Counter />
 			<Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt="shop" width={400} height={400} />
 		</>
